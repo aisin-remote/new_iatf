e5a556ef2e9d470079318dec53f3fe2080dadac1
alessandrofhs <alessandrofhs@gmail.com>
2024-06-05 11:13:16 +0700

    second commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->string('npk')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('departemen');
-            $table->enum('role', ['admin', 'departemen']);
             $table->rememberToken();
             $table->timestamps();
         });
