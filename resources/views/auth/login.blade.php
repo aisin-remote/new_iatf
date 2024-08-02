87c3739f5809a9c32620bf7ed5162769ff50ea3e
alessandrofhs <alessandrofhs@gmail.com>
2024-08-02 22:52:42 +0700

    Save local changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
                     <div class="col-lg-4 mx-auto">
                         <div class="auth-form-light text-left py-5 px-4 px-sm-5">
                             <div class="brand-logo">
-                                <img src="../../images/logo.svg" alt="logo">
+                                <img src="../../images/logo-iatf.png" alt="logo">
                             </div>
                             <h4>Hello! let's get started</h4>
                             <h6 class="font-weight-light">Sign in to continue.</h6>
@@ -73,7 +73,7 @@
     <script src="../../js/settings.js"></script>
     <script src="../../js/todolist.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    
+
     <!-- endinject -->
 </body>
 
